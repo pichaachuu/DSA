e980ac4df417b06810638202564415cc1c57997e
Picha <nafisahaliyah01@gmail.com>
2025-02-13 20:41:34 +0700

    tugas jobsheet 2

--- jobsheet-1/tugas/tugas_2.java
+++ jobsheet-1/tugas/tugas_2.java
@@ -4,46 +4,47 @@ import java.util.*;
 public class tugas_2 {
     public static void main(String[] args) {
         Scanner input13 = new Scanner(System.in);
-        System.out.println("SISTEM PERHITUNGAN KUBUS");
-        System.out.println("1. Volume Kubus\n2. Luas Permukaan Kubus\n3. Keliling Kubus");
-        System.out.print("Pilih Menu: ");
-        int menu = input13.nextInt();
-        System.out.print("Masukkan panjang rusuk kubus: ");
-        int rusuk = input13.nextInt();
-        
-        switch (menu) {
-            case 1:
-                System.out.println("Volume kubus: " + volumeKubus(rusuk));
+        while (true) {
+            System.out.println("SISTEM PERHITUNGAN KUBUS");
+            System.out.println("1. Volume Kubus\n2. Luas Permukaan Kubus\n3. Keliling Kubus");
+            System.out.print("Pilih Menu: ");
+            int menu = input13.nextInt();
+            System.out.print("Masukkan panjang rusuk kubus: ");
+            int rusuk = input13.nextInt();
+            
+            if (menu == 4) {
+                System.out.println("Terima kasih telah menggunakan program!");
                 break;
-            case 2:
-                System.out.println("Luas permukaan kubus: " + lpkubus(rusuk));
-                break;
-            case 3:
-                System.out.println("Keliling kubus: " + kelilingKubus(rusuk));
-                break;
-            default:
-                System.out.println("Silahkan masukkan angka yang tepat");
-                break;
+            }
+
+            switch (menu) {
+                case 1:
+                    System.out.println("Volume kubus: " + volumeKubus(rusuk));
+                    break;
+                case 2:
+                    System.out.println("Luas permukaan kubus: " + lpkubus(rusuk));
+                    break;
+                case 3:
+                    System.out.println("Keliling kubus: " + kelilingKubus(rusuk));
+                    break;
+                default:
+                    System.out.println("Silahkan masukkan angka yang tepat");
+                    break;
+            }
         }
 
         input13.close();
     }
 
     public static int volumeKubus (int s){
-        int vol;
-        vol = s * s * s;
-        return vol;
+        return s * s * s;
     }
 
     public static int lpkubus (int s){
-        int lp;
-        lp = 6 * s * s;
-        return lp;
+        return 6 * s * s;
     }
 
     public static int kelilingKubus (int s){
-        int keliling;
-        keliling = 12 * s;
-        return keliling;
+        return 12 *s;
     }
 }
