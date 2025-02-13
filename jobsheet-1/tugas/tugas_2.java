934282902cd89016684731735bdee49433e9ded5
Picha <nafisahaliyah01@gmail.com>
2025-02-13 14:45:53 +0700

    tugas jobsheet 1

--- jobsheet-1/tugas/tugas_2.java
+++ jobsheet-1/tugas/tugas_2.java
@@ -5,12 +5,26 @@ public class tugas_2 {
     public static void main(String[] args) {
         Scanner input13 = new Scanner(System.in);
         System.out.println("SISTEM PERHITUNGAN KUBUS");
+        System.out.println("1. Volume Kubus\n2. Luas Permukaan Kubus\n3. Keliling Kubus");
+        System.out.println("Pilih Menu: ");
+        int menu = input13.nextInt();
         System.out.print("Masukkan panjang rusuk kubus: ");
         int rusuk = input13.nextInt();
-
-        System.out.println("Volume kubus: " + volumeKubus(rusuk));
-        System.out.println("Luas permukaan kubus: " + lpkubus(rusuk));
-        System.out.println("Keliling kubus: " + kelilingKubus(rusuk));
+        
+        switch (menu) {
+            case 1:
+                System.out.println("Volume kubus: " + volumeKubus(rusuk));
+                break;
+            case 2:
+                System.out.println("Luas permukaan kubus: " + lpkubus(rusuk));
+                break;
+            case 3:
+                System.out.println("Keliling kubus: " + kelilingKubus(rusuk));
+                break;
+            default:
+                System.out.println("Silahkan masukkan angka yang tepat");
+                break;
+        }
 
         input13.close();
     }
