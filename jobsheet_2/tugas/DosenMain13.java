82e559aa80e5b50def7fcc6dda43a9a6c5519107
Picha <nafisahaliyah01@gmail.com>
2025-02-20 11:56:15 +0700

    jobsheet 2

--- jobsheet_2/tugas/DosenMain13.java
+++ jobsheet_2/tugas/DosenMain13.java
@@ -6,5 +6,15 @@ public class DosenMain13 {
         dosen1.tampilInformasi(2025);
         dosen1.setStatusAktif(false);
         dosen1.tampilInformasi(2025);
+
+        Dosen13 dosen2 = new Dosen13();
+        dosen1.nama = "April";
+        dosen2.idDosen = "432ER";
+        dosen2.statusAktif = false;
+        dosen2.tahunBergabung = 2020;
+        dosen2.bidangKeahlian = "Artificial Intelligence";
+        dosen2.tampilInformasi(2025);
+        dosen2.setStatusAktif(true);
+        dosen2.tampilInformasi(2025);
     }
 }
