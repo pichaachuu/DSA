923a7a023f5a829c90df721ca2a2fe2863f6d394
Picha <nafisahaliyah01@gmail.com>
2025-02-20 09:44:13 +0700

    jobsheet 2

---
+++ jobsheet_2/MataKuliah13.java
@@ -0,0 +1,44 @@
+package jobsheet_2;
+
+public class MataKuliah13 {
+    String kodeMK;
+    String nama;
+    int sks;
+    int jumlahJam;
+
+    public MataKuliah13 () {
+
+    }
+
+    public MataKuliah13(String namaMatkul, String kodeMatkul, int sks, int jam){
+        kodeMK = kodeMatkul;
+        nama = namaMatkul;
+        this.sks = sks;
+        jumlahJam = jam;
+    }
+
+    void tampilInformasi(){
+        System.out.println("Mata Kuliah: " + nama);
+        System.out.println("Kode Mata Kuliah: " + kodeMK);
+        System.out.println("Jumlah SKS: " + sks + "SKS");
+        System.out.println("Jumlah Jam: " + jumlahJam);
+    }
+
+    void ubahSKS(int sksBaru){
+        sks = sksBaru;
+        System.out.println("SKS telah diubah");
+    }
+
+    void tambahJam(int jam){
+        jumlahJam += jam;
+    }
+
+    void kurangJam(int jam){
+        if (jumlahJam >= jam) {
+            jumlahJam -= jam;
+            System.out.println("Jumlah jam telah dikurangi");
+        } else {
+            System.out.println("Jam tidak dapat dikurangi");
+        }
+    }
+}
---
+++ jobsheet_2/MataKuliahMain13.java
@@ -0,0 +1,8 @@
+package jobsheet_2;
+
+public class MataKuliahMain13 {
+    MataKuliah13 mk1 = new MataKuliah13();
+
+    mk1.nama = "ASD";
+
+}
