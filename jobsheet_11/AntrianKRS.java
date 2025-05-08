0678c531b42e8f815874e0bca3d5fe0a1d3e8ec7
Picha <nafisahaliyah01@gmail.com>
2025-05-08 10:44:49 +0700

    Tugas

---
+++ jobsheet_11/AntrianKRS.java
@@ -0,0 +1,128 @@
+package jobsheet_11;
+
+public class AntrianKRS {
+    Mahasiswa[] data;
+    int front;
+    int rear;
+    int size;
+    int max;
+    int jmlDilayani = 0;
+
+    public AntrianKRS(int max){
+        this.max = max;
+        this.data = new Mahasiswa[max];
+        this.front = 0;
+        this.rear = -1;
+        this.size = 0;
+    }
+
+    public boolean isEmpty(){
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isFull(){
+        if (size == max) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void clear(){
+        if (!isEmpty()) {
+            front = rear = -1;
+            size = 0;
+            System.out.println("Antrian berhasil dikosongkan");
+        } else {
+            System.out.println("Antrian masih kosong");
+        }
+    }
+
+    public void tambahAntrian(Mahasiswa mhs){
+        if (isFull()) {
+            System.out.println("Antrian penuh, tidak dapat menambah mahasiswa.");
+            return;
+        }
+        rear = (rear + 1) % max;
+        data[rear] = mhs;
+        size++;
+        System.out.println(mhs.nama + " berhasil masuk antrian");
+    }
+
+    public Mahasiswa layaniMahasiswa(){
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+        }
+        Mahasiswa mhs = data[front];
+        front = (front + 1) % max;
+        size--;
+        return mhs;
+    }
+
+    public void prosesKRS2Mahasiswa() {
+        if (size < 2) {
+            System.out.println("Minimal 2 mahasiswa dibutuhkan untuk memproses KRS.");
+            return;
+        }
+    
+        System.out.println("Memproses KRS untuk 2 mahasiswa:");
+        for (int i = 0; i < 2; i++) {
+            Mahasiswa mhs = layaniMahasiswa();
+            System.out.print(" - ");
+            mhs.tampilkanData();
+            jmlDilayani++;
+        }
+    }
+
+    public void tampilkanSemua(){
+        if (isEmpty()) {
+            System.out.println("Queue masih kosong");
+            return;
+        }
+        System.out.println("Daftar Mahasiswa dalam Antrian:");
+        System.out.println("NIM - NAMA - PRODI - KELAS");
+        for (int i = 0; i < size; i++) {
+            int index = (front + i) % max;
+            System.out.print((i+1) + ". ");
+            data[index].tampilkanData();
+        }
+    }
+
+    public void lihat2Terdepan(){
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+        } else {
+            System.out.println("Mahasiswa terdepan: ");
+            System.out.println("NIM - NAMA - PRODI - KELAS");
+            for (int i = front; i < front+2; i++) {
+                data[i].tampilkanData();
+            }
+        }
+    }
+
+    public void lihatAkhir(){
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+        } else {
+            System.out.println("Mahasiswa terdepan: ");
+            System.out.println("NIM - NAMA - PRODI - KELAS");
+            data[rear].tampilkanData();
+        }
+    }
+
+    public int getJumlahAntrian(){
+        return size;
+    }
+
+    public int getJmlDilayani(){
+        return jmlDilayani;
+    }
+
+    public int getSisaLayanan(){
+        return 30 - jmlDilayani;
+    }
+}
---
+++ jobsheet_11/LayananKRSMain.java
@@ -0,0 +1,65 @@
+package jobsheet_11;
+import java.util.*;
+
+public class LayananKRSMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        AntrianKRS antrian = new AntrianKRS(10);
+        int pilihan;
+
+        do {
+            System.out.println("\n=== Menu Antrian Layanan KRS ==-");
+            System.out.println("1. Tambah Mahasiswa ke Antrian");
+            System.out.println("2. Layani 2 Mahasiswa");
+            System.out.println("3. Lihat Semua Antrian");
+            System.out.println("4. Lihat 2 Mahasiswa Terdepan");
+            System.out.println("5. Cek antrian paling belakang");
+            System.out.println("6. Jumlah Mahasiswa dalam Antrian");
+            System.out.println("7. Jumlah Mahasiswa yang sudah diproses");
+            System.out.println("8. Jumlah Mahasiswa yang belum melakukan proses KRS");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih menu: ");
+            pilihan = sc.nextInt();
+            sc.nextLine();
+
+            switch (pilihan) {
+                case 1:
+                    System.out.print("NIM       : ");
+                    String nim = sc.nextLine();
+                    System.out.print("Nama      : ");
+                    String nama = sc.nextLine();
+                    System.out.print("Prodi     : ");
+                    String prodi = sc.nextLine();
+                    System.out.print("Kelas     : ");
+                    String kelas = sc.nextLine();
+                    Mahasiswa mhs = new Mahasiswa(nim, nama, prodi, kelas);
+                    antrian.tambahAntrian(mhs);
+                    break;
+                case 2:
+                    antrian.prosesKRS2Mahasiswa();
+                    break;
+                case 3:
+                    antrian.tampilkanSemua();
+                    break; 
+                case 4:
+                    antrian.lihat2Terdepan();
+                    break;
+                case 5:
+                    antrian.lihatAkhir();
+                    break;
+                case 6:
+                    System.out.println("Jumlah mahasiswa dalam antrian: " + antrian.getJumlahAntrian());
+                    break;
+                case 7:
+                    System.out.println("Jumlah mahasiswa yang sudah diproses: " + antrian.getJmlDilayani());
+                    break;
+                case 8:
+                    System.out.println("Jumlah mahasiswa yang belum melakukan KRS: " + antrian.getSisaLayanan());
+                    break;                                     
+                default:
+                    break;
+            }
+        } while (pilihan != 0);
+        sc.close();
+    }    
+}
